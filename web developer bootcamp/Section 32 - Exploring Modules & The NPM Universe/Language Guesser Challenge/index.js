7831488a44f0211b8d481a015933aff2d0acf909
andriusdude <andriusdude@gmail.com>
2025-05-24 17:32:27 -0600

    language guesser

--- web developer bootcamp/Section 32 - Exploring Modules & The NPM Universe/Language Guesser Challenge/index.js
+++ web developer bootcamp/Section 32 - Exploring Modules & The NPM Universe/Language Guesser Challenge/index.js
@@ -1,7 +1,14 @@
 const franc = require("franc");
 const langs = require("langs");
+const colors = require("colors");
 
-const langCode = franc('Alle menslike wesens word vry');
-const language = langs.where("3", langCode);
+const input = process.argv[2];
+const langCode = franc(input);
+if (langCode === 'und') {
+    console.log("Sorry, couldn't figure it out, try with more sample text!".red)
+} else {
 
-console.log(language.name)
+    const language = langs.where("3", langCode);
+    console.log(`Our best guess is: ${language.name}`.green)
+
+}
