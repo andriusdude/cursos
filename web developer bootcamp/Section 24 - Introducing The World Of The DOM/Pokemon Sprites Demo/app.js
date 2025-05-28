516adbff2d0fff1b2b39431fa02affad8af83c0f
andriusdude <andriusdude@gmail.com>
2025-05-27 21:55:19 -0600

    added logitc for creating one single pokemon

--- web developer bootcamp/Section 24 - Introducing The World Of The DOM/Pokemon Sprites Demo/app.js
+++ web developer bootcamp/Section 24 - Introducing The World Of The DOM/Pokemon Sprites Demo/app.js
@@ -1,1 +1,5 @@
 const container = document.querySelector('#container');
+
+const newImg = document.createElement('img');
+newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
+container.appendChild(newImg);
