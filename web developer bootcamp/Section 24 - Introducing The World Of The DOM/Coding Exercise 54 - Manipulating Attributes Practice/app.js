e19f2cf1586f87c6c68516583ac57502658362a3
andriusdude <andriusdude@gmail.com>
2025-05-30 16:22:09 -0600

    coding exercises 54 55 56 and 57

---
+++ web developer bootcamp/Section 24 - Introducing The World Of The DOM/Coding Exercise 54 - Manipulating Attributes Practice/app.js
@@ -0,0 +1,7 @@
+// The url you need: 'https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200'
+
+
+const eggPic = document.querySelector("img");
+
+eggPic.src = 'https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200';
+eggPic.alt = "chicken";
---
+++ web developer bootcamp/Section 24 - Introducing The World Of The DOM/Coding Exercise 55 - Magical Forest Circle Exercise/app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+container.style.textAlign = "center";
+
+const image = document.querySelector('img');
+
+image.style.width = '150px';
+image.style.borderRadius = '50%';
