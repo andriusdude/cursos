ffad7f8291c06b78d22d9a23b39db92404ec6dd8
andriusdude <andriusdude@gmail.com>
2025-05-29 18:50:56 -0600

    click events exercise

---
+++ web developer bootcamp/Section 25 - The Missing Piece DOM Events/Coding Exercise 60 - Click Events Exercise/app.js
@@ -0,0 +1,10 @@
+
+const helloButton = document.querySelector('#hello');
+helloButton.addEventListener('click', function () {
+    console.log('hello');
+});
+
+const goodbyeButton = document.querySelector('#goodbye');
+goodbyeButton.addEventListener('click', function () {
+    console.log('goodbye');
+});
